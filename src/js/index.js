a4e33cc777e2d4bfd823d5f5670df92ff8b6cbcf
Vukas82 <vukas82@gmail.com>
2020-01-14 22:55:19 +0100

    first

---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+// // navabar
+// function toggle(id, id2) {
+//     // console.log(btn.classList.value)
+
+//     if (btn.classList.value === "small-drop-btn") {
+//         btn.classList.add('is-active');
+//     } else {
+//         btn.classList.remove('is-active');
+//     }
+
+//     let n = document.getElementById(id);
+//     if (n.style.display != 'none') {
+//         n.style.display = 'none';
+//         document.getElementById(id2).setAttribute('aria-expanded', 'true');
+//     } else {
+//         n.style.display = '';
+//         document.getElementById(id2).setAttribute('aria-expanded', 'false');
+//     }
+// }
+
+
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function () {
+//     stickyHeader()
+// };
+
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyHeader() {
+//     // Get the header
+//     var header = document.getElementById("myHeader");
+//     var slider = document.querySelector(".slider");
+
+//     // Get the offset position of the navbar
+//     var sticky = header.offsetTop + 500;
+
+//     // console.log('scroll')
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//         slider.style.marginTop = '60px';
+//     } else {
+//         header.classList.remove("sticky");
+//         slider.style.marginTop = '0px';
+
+//     }
+// }
+
+// //-----------------------------------------
+// // navabar
+// function toggle(id, id2) {
+//     // console.log(btn.classList.value)
+
+//     if (btn.classList.value === "small-drop-btn") {
+//         btn.classList.add('is-active');
+//     } else {
+//         btn.classList.remove('is-active');
+//     }
+
+//     let n = document.getElementById(id);
+//     if (n.style.display != 'none') {
+//         n.style.display = 'none';
+//         document.getElementById(id2).setAttribute('aria-expanded', 'true');
+//     } else {
+//         n.style.display = '';
+//         document.getElementById(id2).setAttribute('aria-expanded', 'false');
+//     }
+// }
+
+// // ----------------------------------------------
+
+
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyHeaderRefense() {
+//     // Get the header
+//     var header = document.getElementById("myHeader");
+//     var slider = document.querySelector(".viewport");
+
+//     // Get the offset position of the navbar
+//     var sticky = header.offsetTop + 330;
+
+//     // console.log('scroll')
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//         slider.style.marginTop = '60px';
+//     } else {
+//         header.classList.remove("sticky");
+//         slider.style.marginTop = '0px';
+
+//     }
+// }
+
+console.log('est')
