07cbe46e12659adf898d7cacdaec4bca06142d43
Vukas82 <vukas82@gmail.com>
2020-01-28 21:38:57 +0100

    tl

--- gulpFile.js
+++ gulpFile.js
@@ -1,8 +1,18 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
+// const uglify = require('gulp-uglify-es');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+
+// const uglifyes = require('gulp-uglify-es');
+// const composer = require('gulp-uglify/composer');
+// const uglify = composer(uglifyes, console);
+
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+const autoprefixer = require('gulp-autoprefixer');
+
 /*
     --TOP LEVEL FUNCTIONS--
     gulp.task - Define task
@@ -15,6 +25,7 @@ const concat = require('gulp-concat');
 gulp.task('message', function () {
     return console.log('gulp is runing...');
 });
+
 
 
 //Copy All HTML fils
@@ -37,14 +48,14 @@ gulp.task("imageMin", async () => {
 });
 
 //Minify js
-// gulp.task("minify", async () => {
-//     await new Promise((resolve, reject) => {
-//         gulp.src('src/js/*.js')
-//             .pipe(uglify())
-//             .pipe(gulp.dest('dist/js'))
-//             .on("end", resolve);
-//     });
-// });
+gulp.task("minify", async () => {
+    await new Promise((resolve, reject) => {
+        gulp.src('src/js/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('dist/js'))
+            .on("end", resolve);
+    });
+});
 
 //Compile Sass
 gulp.task("sass", async () => {
@@ -56,27 +67,90 @@ gulp.task("sass", async () => {
     });
 });
 
+
+
+// gulp.task("vendor", async () => {
+//     await new Promise((resolve, reject) => {
+//         gulp.src('dist/css/style.css')
+//             .pipe(autoprefixer({
+//                 cascade: false
+//             }))
+//             .pipe(gulp.dest('dist/css/vendor'))
+//             .on("end", resolve);
+//     });
+// });
+
+
+// gulp.task('scripts', function () {
+//     return gulp.src('src/js/*.js')
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist/js'))
+// });
 // Scripts
+// gulp.task("scripts", async () => {
+//     await new Promise((resolve, reject) => {
+//         gulp.src("src/js/*.js")
+//             .pipe(concat('main.js'))
+//             .pipe(uglify())
+//             .pipe(gulp.dest('dist/js'))
+//             .on("end", resolve);
+//     });
+// });
+
+
+
+// gulp.task('script', () => {
+//     return gulp.src('src/**/*.js')
+//         .pipe(babel({
+//             presets: ['@babel/env']
+//         }))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('src/dist'))
+// });
+
 gulp.task("scripts", async () => {
     await new Promise((resolve, reject) => {
         gulp.src("src/js/*.js")
-            .pipe(concat('index.js'))
+            .pipe(concat('main.js'))
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
             .pipe(uglify())
             .pipe(gulp.dest('dist/js'))
             .on("end", resolve);
     });
 });
 
-//Gulp watch al process function
-// gulp.task('watch', () => {
-//     gulp.watch(
-//         ['src/images/*', 'src/sass/*.scss', 'src/*.html'],
-//         gulp.series(['imageMin', 'sass', 'copyHtml'])
-//     );
+// Scripts
+// gulp.task("scripts", async () => {
+//     await new Promise((resolve, reject) => {
+//         gulp.src("src/js/*.js")
+//             .pipe(concat('main.js'))
+//             .pipe(uglify())
+//             .pipe(gulp.dest('dist/js'))
+//             .on("end", resolve);
+//     });
 // });
+
+//Gulp watch al CSS / html process function
 gulp.task('watch', () => {
     gulp.watch(
-        ['src/js/*.js', 'src/sass/*.scss', 'src/*.html'],
-        gulp.series(['scripts', 'sass', 'copyHtml'])
+        ['src/sass/*.scss', 'src/*.html'],
+        gulp.series(['sass', 'copyHtml'])
+    );
+});
+
+//Gulp watch al JS process function
+gulp.task('watchJs', () => {
+    gulp.watch(
+        ['src/js/*.js', 'src/*.html'],
+        gulp.series(['scripts', 'copyHtml'])
+    );
+});
+// //Gulp watch all process function  originalsetup
+gulp.task('watchAll', () => {
+    gulp.watch(
+        ['src/js/*.js', 'src/images/*', 'src/sass/*.scss', 'src/*.html'],
+        gulp.series(['scripts', 'imageMin', 'sass', 'copyHtml'])
     );
 });
--- src/js/index.js
+++ src/js/index.js
@@ -90,4 +90,146 @@
 //     }
 // }
 
-console.log('est')
+// var buttons = document.querySelectorAll(".btn_toogle");
+// // console.log(button)
+// buttons.forEach(function (button) {
+
+//     button.addEventListener('click', function () {
+//         console.log(this.className)
+//         if (this.className === "btn_shadow_in") {
+//             this.className = "btn_shadow_out";
+//         } else {
+//             this.className = "btn_shadow_in";
+//         }
+//     })
+// })
+
+
+//text tipping
+//    const TypeWriter = function (txtElement, words, wait = 3000) {
+//     this.txtElement = txtElement;
+//     this.words = words;
+//     this.txt = '';
+//     this.wordIndex = 0;
+//     this.wait = parseInt(wait, 10);
+//     this.type();
+//     this.isDeleting = false;
+// }
+
+// Type method
+// TypeWriter.prototype.type = function () {
+//     // current index of word
+//     const current = this.wordIndex % this.words.length;
+
+//     //get full txt of current word
+//     const fullText = this.words[current];
+
+//     // console.log(fullText)
+//     // check if deleting
+//     if (this.isDeleting) {
+//         //REMOVE CHAR
+//         this.txt = fullText.substring(0, this.txt.length - 1)
+//     } else {
+//         // add char
+//         this.txt = fullText.substring(0, this.txt.length + 1)
+//     }
+
+//     // insert txt into element
+//     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+//     // Initial type speed
+//     let typeSpeed = 300;
+
+//     if (this.isDeleting) {
+//         typeSpeed /= 2;
+//     }
+
+//     // If word is complete
+//     if (!this.isDeleting && this.txt === fullText) {
+//         // make pause on the end
+//         typeSpeed = this.wait;
+
+//         this.isDeleting = true
+//     } else if (this.isDeleting && this.txt === '') {
+//         this.isDeleting = false;
+//         // move to next word
+//         this.wordIndex++;
+//         // pause before start typing]
+//         typeSpeed = 500;
+//     }
+
+//     setTimeout(() => this.type(), typeSpeed);
+
+// }
+
+
+
+
+
+// class TypeWriter {
+//     constructor(txtElement, words, wait = 3000) {
+//         this.txtElement = txtElement;
+//         this.words = words;
+//         this.txt = '';
+//         this.wordIndex = 0;
+//         this.wait = parseInt(wait, 10);
+//         this.type();
+//         this.isDeleting = false;
+//     }
+
+//     type() {
+//         // current index of word
+//         const current = this.wordIndex % this.words.length;
+
+//         //get full txt of current word
+//         const fullText = this.words[current];
+
+//         // console.log(fullText)
+//         // check if deleting
+//         if (this.isDeleting) {
+//             //REMOVE CHAR
+//             this.txt = fullText.substring(0, this.txt.length - 1)
+//         } else {
+//             // add char
+//             this.txt = fullText.substring(0, this.txt.length + 1)
+//         }
+
+//         // insert txt into element
+//         this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+//         // Initial type speed
+//         let typeSpeed = 300;
+
+//         if (this.isDeleting) {
+//             typeSpeed /= 2;
+//         }
+
+//         // If word is complete
+//         if (!this.isDeleting && this.txt === fullText) {
+//             // make pause on the end
+//             typeSpeed = this.wait;
+
+//             this.isDeleting = true
+//         } else if (this.isDeleting && this.txt === '') {
+//             this.isDeleting = false;
+//             // move to next word
+//             this.wordIndex++;
+//             // pause before start typing]
+//             typeSpeed = 500;
+//         }
+
+//         setTimeout(() => this.type(), typeSpeed);
+//     }
+// }
+
+
+// // Init on dom load
+// document.addEventListener('DOMContentLoaded', init);
+// // Inigt app
+// function init() {
+//     const txtElement = document.querySelector('.txt-type');
+//     const words = JSON.parse(txtElement.getAttribute('data-words'));
+//     const wait = txtElement.getAttribute('data-wait');
+//     //init TypeWriter
+//     new TypeWriter(txtElement, words, wait)
+// }
